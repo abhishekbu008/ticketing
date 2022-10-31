330f5b264edf3908b8740cca15213f9b0d85133b
abhishekbu008 <abhishekbu30@gmail.com>
2022-10-31 22:45:03 +0530

    initial commit and Added occ

---
+++ auth/src/app.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import cookieSession from "cookie-session";
+
+import { currentUserRouter } from "./routes/current-user";
+import { signInRouter } from "./routes/signin";
+import { signOutRouter } from "./routes/signout";
+import { signUpRouter } from "./routes/signup";
+import { errorHandler, NotFoundError } from "@bu.ticketing/common";
+
+const app = express();
+app.set("trust proxy", true);
+app.use(express.json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== "test",
+  })
+);
+
+app.use(currentUserRouter);
+app.use(signInRouter);
+app.use(signUpRouter);
+app.use(signOutRouter);
+
+app.get("*", async () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
---
+++ tickets/src/events/publishers/ticket-updated-publisher.ts
@@ -0,0 +1,5 @@
+import { Publisher, Subjects, TicketUpdatedEvent } from "@bu.ticketing/common";
+
+export class TicketUpdatedPublisher extends Publisher<TicketUpdatedEvent> {
+  subject: Subjects.TicketUpdated = Subjects.TicketUpdated;
+}
